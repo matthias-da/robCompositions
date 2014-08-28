0d1d4091f79f1c2c52a075c91c4fb30bf81dbfa7
Matthias Templ <matthias.templ@gmail.com>
2014-08-28 10:06:23 +0200

    Update isomLR.R
    fast version included.

--- R/isomLR.R
+++ R/isomLR.R
@@ -1,9 +1,15 @@
-"isomLR" <- function(x){
+"isomLR" <- function(x, fast=FALSE){
 	x.ilr=matrix(NA,nrow=nrow(x),ncol=ncol(x)-1)
 	D=ncol(x)
-	for (i in 1:ncol(x.ilr)){
+        if(fast){
+	  for (i in 1:ncol(x.ilr)){
+	     x.ilr[,i]=sqrt((D-i)/(D-i+1))*log(((apply(as.matrix(x[,(i+1):D,drop=FALSE]),1,prod))^(1/(D-i)))/(x[,i]))
+	  }
+	} else {
+  	  for (i in 1:ncol(x.ilr)){
 #		x.ilr[,i]=sqrt((D-i)/(D-i+1))*log(((apply(as.matrix(x[,(i+1):D,drop=FALSE]),1,prod))^(1/(D-i)))/(x[,i]))
 		x.ilr[,i]=sqrt((D-i)/(D-i+1))*log(apply(as.matrix(x[,(i+1):D]), 1, gm)/(x[,i]))	
-	} 
+	  } 
+	}
 	return(x.ilr)
 }
