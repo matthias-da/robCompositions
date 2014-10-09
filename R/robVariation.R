ba8d1c8922d6d0369a73091e443f131e97ecc204
Matthias Templ <matthias.templ@gmail.com>
2014-10-09 16:38:08 +0200

    Update robVariation.R
    bug in robVariation solved.

--- R/robVariation.R
+++ R/robVariation.R
@@ -1,20 +1,24 @@
 `robVariation` <-
-function(x, robust=TRUE){
+  function(x, robust=TRUE){
     rvars <- matrix(0, ncol=ncol(x), nrow=ncol(x))
-	if(robust){
-    for( i in 1:ncol(x)){
-      for( j in 1:ncol(x)){
-        if( i < j ) rvars[i,j] <- (mad(log(x[,i]/x[,j])))^2
+    if(robust){
+      for( i in 1:ncol(x)){
+        for( j in 1:ncol(x)){
+          if( i < j ){
+            rvars[i,j] <- (mad(log(x[,i]/x[,j])))^2
+            rvars[j,i] <- rvars[i,j]
+          }
+        }
       }
+    } else{
+      for( i in 1:ncol(x)){
+        for( j in 1:ncol(x)){
+          if( i < j ){ 
+            rvars[i,j] <- (var(log(x[,i]/x[,j])))
+            rvars[j,i] <- rvars[i,j]            
+          }
+        }
+      }		
     }
-	} else{
-		for( i in 1:ncol(x)){
-			for( j in 1:ncol(x)){
-				if( i < j ) rvars[i,j] <- (var(log(x[,i]/x[,j])))
-			}
-		}		
-	}
-	rvars[lower.tri(rvars)] <- rvars[upper.tri(rvars)]
     return(rvars) 
 }
-
