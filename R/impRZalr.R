b8573b3a7b29271f2ce62c56c87116d2ade5586e
matthias99 <matthias.templ@gmail.com>
2015-02-04 17:46:53 +0100

    lar

--- R/impRZalr.R
+++ R/impRZalr.R
@@ -1,6 +1,7 @@
 impRZalr <- function(x, pos=ncol(x), dl=rep(0.05, ncol(x)-1), 
                      eps=0.0001, maxit=50, bruteforce=FALSE, 
-                     method="lm", step=FALSE) {
+                     method="lm", step=FALSE, nComp = "boot", R=10,
+                     verbose=FALSE) {
 
 #   x <- xorig <- genVarsSimple(n=50,p=30)
 #   qu <- apply(x, 2, quantile, 0.05)
@@ -18,7 +19,19 @@ impRZalr <- function(x, pos=ncol(x), dl=rep(0.05, ncol(x)-1),
   if(is.matrix(x)) stop("convert to data.frame first")
   stopifnot(all(x[,pos] != 0 & length(which(is.na(x[,pos]))) == 0))
   if(!any(is.na(x)) && !any(x==0) ) stop("No zeros or missing values in the data")
-
+  if(method=="pls" & ncol(x)<5) stop("too less variables/parts for method pls")
+  if(is.null(nComp)){
+    pre <- FALSE
+    nC <- NULL
+  } else if(nComp=="boot"){
+    nC <- integer(ncol(x))
+    pre <- TRUE
+  } else if(length(nComp) == ncol(x)){
+    nC <- nComp
+    pre <- FALSE
+  } else  {
+    pre <- FALSE	
+  }
   
   ## zeros to NA:
   x[x==0] <- NA
@@ -48,31 +61,49 @@ impRZalr <- function(x, pos=ncol(x), dl=rep(0.05, ncol(x)-1),
 	yold <- xax
   n2 <- nrow(xax)-ncol(xax)+1
   cn <- colnames(x)
+  n <- nrow(x)
   for(i in 1:ncol(xax)){ 
     response <- xax[,i]
     predictors <- as.matrix(xax[,-i])
 		if(method=="lm" && !step){
 	    lm1 <- lm(response ~ predictors)
   			yhat <- predict(lm1, new.data=predictors)	  
-  #			s <- sd(lm1$res, na.rm=TRUE)
-	  		s <- sqrt(sum(lm1$res^2)) / n2	
-	    }
-	    if(method=="lm" && step){
-		  	lm1 <- lm(response ~ predictors) 
-		    lm1 <- stepAIC(lm1, trace=FALSE)
-		  	yhat <- predict(lm1, new.data=predictors)	  
-  #			s <- sd(lm1$res, na.rm=TRUE)
-  			s <- sqrt(sum(lm1$res^2)) / n2			
-	  	}
-      if(method=="MM" && !step) {
-  			lm1 <- lmrob(response ~ predictors)
-  			yhat <- predict(lm1, new.data=predictors)	
-  			if(any(is.na(yhat))) stop("NA in yhat")
-  			if(any(yhat=="Inf")) stop("Inf in yhat")	
-  			s <- lm1$s		
-  		}
-	    if(method=="MM" && step) {
-		   stop("robust + stepwise is not implemented until now.")
+  			s <- sd(lm1$res, na.rm=TRUE)
+	#  		s <- sqrt(sum(lm1$res^2)) / n2	
+	  }
+    if(method=="pls" && !step){    
+      if(it == 1 & pre){ ## evaluate ncomp.
+        nC[i] <- bootnComp(predictors,
+                           y=response, R, 
+                           plotting=FALSE)$res #$res2
+      }
+      if(verbose) cat("\n ncomp for part",i,":",nC[i])
+      reg1 <- mvr(as.matrix(response) ~ as.matrix(predictors), ncomp=nC[i],
+                  method="simpls")
+      yhat <- predict(reg1, new.data=data.frame(predictors), ncomp=nC[i])
+      s <- sqrt(sum(reg1$res^2)/n) 
+      
+      lm1 <- lm(response ~ predictors)
+      yhat <- predict(lm1, new.data=predictors)	  
+    		s <- sd(lm1$res, na.rm=TRUE)
+      # s <- sqrt(sum(lm1$res^2)) / n2	
+    }
+    if(method=="lm" && step){
+	  	lm1 <- lm(response ~ predictors) 
+	    lm1 <- stepAIC(lm1, trace=FALSE)
+	  	yhat <- predict(lm1, new.data=predictors)	  
+    	s <- sd(lm1$res, na.rm=TRUE)
+  	#	s <- sqrt(sum(lm1$res^2)) / n2			
+	 	}
+    if(method=="MM" && !step) {
+  		lm1 <- lmrob(response ~ predictors)
+  		yhat <- predict(lm1, new.data=predictors)	
+  		if(any(is.na(yhat))) stop("NA in yhat")
+  		if(any(yhat=="Inf")) stop("Inf in yhat")	
+  		s <- lm1$s		
+  	}
+	  if(method=="MM" && step) {
+	    stop("robust + stepwise is not implemented until now.")
 	   }	
 	  ex <- (phi[,i] - yhat)/s 
 		tobit <- s*dnorm(ex)/pnorm(ex)
@@ -97,7 +128,7 @@ impRZalr <- function(x, pos=ncol(x), dl=rep(0.05, ncol(x)-1),
   ximp <- suppressWarnings(addLRinv(xa)) 
   
   ## result:
-  res <- list(xOrig=xOrig, xImp=ximp, wind=NULL, iter=it, eps=eps) 
+  res <- list(x=ximp, wind=NULL, iter=it, eps=eps) 
   invisible(res)
 }
 
