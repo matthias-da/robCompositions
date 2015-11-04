847885565df3729f6a85079f30b3c4bd7ef4c754
matthias99 <matthias.templ@gmail.com>
2015-11-04 09:56:25 +0100

    new pfa, new plots for outCoDa

--- R/outCoDa.R
+++ R/outCoDa.R
@@ -29,6 +29,8 @@
 #' default is (n+p+1)/2).
 #' @param y unused second plot argument for the plot method
 #' @param ... additional parameters for print and plot method passed through
+#' @param isometric log-ratio transformation or no transformation or self-defined function.
+#' For a self-defined function a data.frame with the transformed variables should be returned.
 #' @return \item{mahalDist }{resulting Mahalanobis distance} \item{limit
 #' }{quantile of the Chi-squared distribution} \item{outlierIndex }{logical
 #' vector indicating outliers and non-outliers} \item{method }{method used}
@@ -51,8 +53,11 @@
 #' data(expenditures)
 #' oD <- outCoDa(expenditures)
 #' oD
+#' ## providing a function:
+#' oD <- outCoDa(expenditures, coda = log)
 #' 
-outCoDa <- function(x, quantile=0.975, method="robust", h=1/2){
+outCoDa <- function(x, quantile=0.975, method="robust", 
+                    h=1/2, coda = TRUE){
 	if(dim(x)[2] < 2) stop("need data with at least 2 variables")
 	
 	covEst <- function(x, type) {
@@ -67,13 +72,23 @@ outCoDa <- function(x, quantile=0.975, method="robust", h=1/2){
 				standard = standard(x),
 				robust = robust(x))
 	}
-		
-	z <- isomLR(x)
-	cv <- covEst(z, method)
-	dM <- sqrt(mahalanobis(z, center=cv$mean, cov=cv$varmat))
+	if(!is.logical(coda) & !is.function(coda)){
+	  stop("coda must be logical or function")
+	}
+	if(!is.logical(coda)){
+	  x <- coda(x)
+	}	else if (coda){ 
+	  x <- isomLR(x)
+	}  
+	cv <- covEst(x, "robust")
+	cvc <- covEst(x, "standard")
+	dM <- sqrt(mahalanobis(x, center=cv$mean, cov=cv$varmat))
+	dMc <- sqrt(mahalanobis(x, center=cvc$mean, cov=cvc$varmat))
 	limit <- sqrt(qchisq(p=quantile, df=ncol(x)-1))
 	res <- list(mahalDist = dM, limit = limit, 
-			    outlierIndex = dM > limit, method=method)
+			    outlierIndex = dM > limit, method=method, 
+			    om2 = dMc > limit,
+			    m2=dMc, coda = coda)
 	class(res) <- "outCoDa"
     invisible(res)
 }
@@ -89,15 +104,40 @@ print.outCoDa <- function(x, ...){
 #' @rdname outCoDa 
 #' @export
 #' @method plot outCoDa
-plot.outCoDa <- function(x, y, ...){
+#' @param which 1 ... MD against index
+#' 2 ... distance-distance plot
+plot.outCoDa <- function(x, y, ..., which = 1){
   #	plot(1:length(x$mahalDist), x$mahalDist, ylab="Mahalanobis distance", xlab="Index", type="n", ylim=c(0, max(x$mahalDist)) )
   #	points(1:length(x$mahalDist[x$outlierIndex]), x$mahalDist[x$outlierIndex], pch=3, col="red")
   #	points(1:length(x$mahalDist[!x$outlierIndex]), x$mahalDist[!x$outlierIndex])	
   #	abline(h=x$limit, lty=2)
   #	#print(x$mahalDist)
+  if(which == 1){
   if(x$method =="standard") yl <- "Mahalanobis distance" else yl <- "Robust Mahalanobis distance" 
-  plot(1:length(x$mahalDist), x$mahalDist, ylab = yl,
-       xlab = "Index", ylim = c(0, max(x$mahalDist)),
-       pch = as.numeric(x$outlierIndex)*2+1)
-  abline(h=x$limit, lty=2)
+    plot(1:length(x$mahalDist), x$mahalDist, ylab = yl,
+         xlab = "Index", ylim = c(0, max(x$mahalDist)),
+         pch = as.numeric(x$outlierIndex)*2+1)
+    abline(h=x$limit, lty=2)
+  }
+  if(which == 2){
+    if(x$method == "standard"){
+      return(cat("apply method robust in outCoDa to make a distance-distance plot available"))
+    }
+    n <- length(x$mahalDist)
+    outlier <- ifelse(x$outlierIndex & x$om2, "both", 
+                      ifelse(x$outlierIndex & !x$om2, "robust only",
+                             ifelse(x$om2 & !x$outlierIndex, "standard only", "no outlier")))
+    df <- data.frame("RMD" = x$mahalDist,
+                     "MD" = x$m2,
+                     "limit" = rep(x$limit, n),
+                     "outlier" = outlier,
+                     "name" = 1:n)
+    gg <- ggplot(df, aes(x = MD, y = RMD, colour = outlier )) 
+    gg <- gg + geom_point(data=subset(df, RMD <= limit & MD <= limit)) 
+    gg <- gg + geom_abline(intercept = 0, slope = 1, linetype = "dashed", colour="grey") 
+    gg <- gg + geom_text(data=subset(df, RMD > limit | MD > limit),  aes(MD, RMD, label=name))
+    gg <- gg + geom_vline(xintercept = x$limit, size=0.25) + geom_hline(yintercept = x$limit, size=0.25)
+    gg <- gg + theme_bw()
+    print(gg)
+  }
 }
--- R/pfa.R
+++ R/pfa.R
@@ -53,7 +53,7 @@
 #' 
 #' data(expenditures)
 #' x <- expenditures
-#' res0 <- pfa(x, factors=1, covmat="cov")
+#' res0 <- pfa(x, factors=1)
 #' 
 #' ## the following produce always the same result:
 #' res1 <- pfa(x, factors=1, covmat="covMcd")
@@ -76,133 +76,129 @@ function (x, factors, data = NULL, covmat = NULL, n.obs = NA,
 	}
 	y <- z%*%t(V)  #clr transformed data
 #	if(transformation=="ilr") x <- ilr(x) else if(transformation=="clr") x <- clr(x)$x.clr else stop("This transformation is not supported by pfa().")
-	x <- scale(x,scale=FALSE)
-    sortLoadings <- function(Lambda) {
-        cn <- colnames(Lambda)
-        Phi <- attr(Lambda, "covariance")
-        ssq <- apply(Lambda, 2, function(x) -sum(x^2))
-        Lambda <- Lambda[, order(ssq), drop = FALSE]
-        colnames(Lambda) <- cn
-        neg <- colSums(Lambda) < 0
-        Lambda[, neg] <- -Lambda[, neg]
-        if (!is.null(Phi)) {
-            unit <- ifelse(neg, -1, 1)
-            attr(Lambda, "covariance") <- unit %*% Phi[order(ssq), 
-                order(ssq)] %*% unit
-        }
-        Lambda
-    }
-    cl <- match.call()
-    na.act <- NULL
+	x <- scale(x, scale=FALSE)  ## TODO: check this line if needed! (not in Peters code)
+	sortLoadings <- function(Lambda) {
+	  cn <- colnames(Lambda)
+	  Phi <- attr(Lambda, "covariance")
+	  ssq <- apply(Lambda, 2, function(x) -sum(x^2))
+	  Lambda <- Lambda[, order(ssq), drop = FALSE]
+	  colnames(Lambda) <- cn
+	  neg <- colSums(Lambda) < 0
+	  Lambda[, neg] <- -Lambda[, neg]
+	  if (!is.null(Phi)) {
+	    unit <- ifelse(neg, -1, 1)
+	    attr(Lambda, "covariance") <- unit %*% Phi[order(ssq), 
+	                                               order(ssq)] %*% unit
+	  }
+	  Lambda
+	}
+	cl <- match.call()
+	na.act <- NULL
 	if (is.character(covmat) && length(covmat)==1){
-		if(covmat == "cov"){
-			cv <- cov(z)
-			n.obs <- nrow(x)
-		} else{
-			cv <- get(covmat)(z)$cov
-			n.obs <- nrow(x)
-		}
+	  if(covmat == "cov"){
+	    cv <- cov(z)
+	    n.obs <- nrow(x)
+	  } else{
+	    cv <- get(covmat)(z)$cov
+	    n.obs <- nrow(x)
+	  }
 	} else if (is.list(covmat)) {
-        if (any(is.na(match(c("cov", "n.obs"), names(covmat))))) 
-            stop("covmat is not a valid covariance list")
-        cv <- covmat$cov
-		if(dim(cv)[2] != dim(z)[2]) stop(paste("covariance matrix is not of dimension", dim(z)[2], "times", dim(z)[2], ". Was it really taken from the transformed data?"))
-        n.obs <- covmat$n.obs
-        have.x <- FALSE
-    } else if (is.matrix(covmat)) {
-        cv <- covmat
-		if(dim(cv)[2] != dim(z)[2]) stop(paste("covariance matrix is not of dimension", dim(z)[2], "times", dim(z)[2], ". Was it really taken from the transformed data?"))
-        have.x <- FALSE
-		n.obs <- nrow(x)
-    } else if (is.null(covmat)) {
-        if (missing(x)) 
-            stop("neither x nor covmat supplied")
-        have.x <- TRUE
-        if (inherits(x, "formula")) { ## TODO: mit z!
-            mt <- terms(x, data = data)
-            if (attr(mt, "response") > 0) 
-                stop("response not allowed in formula")
-            attr(mt, "intercept") <- 0
-            mf <- match.call(expand.dots = FALSE)
-            names(mf)[names(mf) == "x"] <- "formula"
-            mf$factors <- mf$covmat <- mf$scores <- mf$start <- mf$rotation <- mf$control <- mf$... <- NULL
-            mf[[1]] <- as.name("model.frame")
-            mf <- eval(mf, parent.frame())
-            na.act <- attr(mf, "na.action")
-            z <- model.matrix(mt, mf)
-        }
-        else {
-            z <- as.matrix(z) ## statt x
-            if (!missing(subset)) 
-                z <- z[subset, , drop = FALSE]
-        }
-        covmat <- cov.wt(z)
-        cv <- covmat$cov
-        n.obs <- covmat$n.obs
-    }
-    else stop("covmat is of unknown type")
-	## end covmat
-	## covmat saved in cv
-	x <- y  ## use clr transformed data!
-    scores <- match.arg(scores)
-    if (scores != "none" && !have.x) 
-        z <- x
-    sds <- sqrt(diag(cv))
-    cv <- cv/(sds %o% sds)
-    p <- ncol(cv)
-    dof <- 0.5 * ((p - factors)^2 - p - factors)
-    cn <- list(nstart = 1, trace = FALSE, lower = 0.005)
-    cn[names(control)] <- control
-    more <- list(...)[c("nstart", "trace", "lower", "opt", "rotate")]
-    if (length(more)) 
-        cn[names(more)] <- more
-    if (is.null(start)) {
-        start <- (1 - 0.5 * factors/p)/diag(solve(cv))
-    }
-    start <- as.matrix(start)
-    if (nrow(start) != p) 
-        stop(paste("start must have", p, "rows"))
-    nc <- ncol(start)
-    if (nc < 1) 
-        stop("no starting values supplied")
-    fit <- factanal.fit.principal1(cv, factors, p = p, start = start[, 
-        1], iter.max = maxiter)
-    load <- fit$loadings
-    if (rotation != "none") {
-        rot <- do.call(rotation, c(list(load), cn$rotate))
-        load <- if (is.list(rot)) 
-            rot$loadings
-        else rot
-    }
-    fit$loadings <- sortLoadings(load)
-    class(fit$loadings) <- "loadings"
-    fit$na.action <- na.act
-    if (scores != "none") {
-        Lambda <- fit$loadings
-        zz <- z
-        switch(scores, regression = {
-            sc <- as.matrix(zz) %*% solve(cv, Lambda)
-            if (!is.null(Phi <- attr(Lambda, "covariance"))) 
-                sc <- sc %*% Phi
-        }, Bartlett = {
-	    psiinv <- MASS::ginv(fit$psi)
-	    sc <- t(MASS::ginv(t(Lambda)%*%psiinv%*%Lambda)%*%t(Lambda)%*%psiinv%*%t(zz))
-###            d <- 1/fit$uniquenesses
-###            tmp <- t(Lambda * d)
-###            sc <- t(solve(tmp %*% Lambda, tmp %*% t(zz)))
-        })
-        rownames(sc) <- rownames(z)
-        colnames(sc) <- colnames(Lambda)
-        if (!is.null(na.act)) 
-            sc <- napredict(na.act, sc)
-        fit$scores <- sc
-    }
-    if (!is.na(n.obs) && dof > 0) {
-        fit$STATISTIC <- (n.obs - 1 - (2 * p + 5)/6 - (2 * factors)/3) * 
-            fit$criteria#["objective"]
-        fit$PVAL <- pchisq(fit$STATISTIC, dof, lower.tail = FALSE)
-    }
-    fit$n.obs <- n.obs
-    fit$call <- cl
-    fit
+	  if (any(is.na(match(c("cov", "n.obs"), names(covmat))))) 
+	    stop("covmat is not a valid covariance list")
+	  cv <- covmat$cov
+	  n.obs <- covmat$n.obs
+	  have.x <- FALSE
+	}
+	else if (is.matrix(covmat)) {
+	  cv <- covmat
+	  have.x <- FALSE
+	}
+	else if (is.null(covmat)) {
+	  if (missing(x)) 
+	    stop("neither x nor covmat supplied")
+	  have.x <- TRUE
+	  if (inherits(x, "formula")) {
+	    mt <- terms(x, data = data)
+	    if (attr(mt, "response") > 0) 
+	      stop("response not allowed in formula")
+	    attr(mt, "intercept") <- 0
+	    mf <- match.call(expand.dots = FALSE)
+	    names(mf)[names(mf) == "x"] <- "formula"
+	    mf$factors <- mf$covmat <- mf$scores <- mf$start <- mf$rotation <- mf$control <- mf$... <- NULL
+	    mf[[1]] <- as.name("model.frame")
+	    mf <- eval(mf, parent.frame())
+	    na.act <- attr(mf, "na.action")
+	    z <- model.matrix(mt, mf)
+	  }
+	  else {
+	    z <- as.matrix(x)
+	    if (!missing(subset)) 
+	      z <- z[subset, , drop = FALSE]
+	  }
+	  covmat <- cov.wt(z)
+	  cv <- covmat$cov
+	  n.obs <- covmat$n.obs
+	}
+	else stop("covmat is of unknown type")
+	scores <- match.arg(scores)
+	if (scores != "none" && !have.x) 
+	  z <- x
+	sds <- sqrt(diag(cv))
+	cv <- cv/(sds %o% sds)
+	p <- ncol(cv)
+	dof <- 0.5 * ((p - factors)^2 - p - factors)
+	cn <- list(nstart = 1, trace = FALSE, lower = 0.005)
+	cn[names(control)] <- control
+	more <- list(...)[c("nstart", "trace", "lower", "opt", "rotate")]
+	if (length(more)) 
+	  cn[names(more)] <- more
+	if (is.null(start)) {
+	  start <- (1 - 0.5 * factors/p)/diag(solve(cv))
+	}
+	start <- as.matrix(start)
+	if (nrow(start) != p) 
+	  stop(paste("start must have", p, "rows"))
+	nc <- ncol(start)
+	if (nc < 1) 
+	  stop("no starting values supplied")
+	fit <- factanal.fit.principal1(cv, factors, p = p, start = start[, 
+	                                                                 1], iter.max = maxiter)
+	load <- fit$loadings
+	if (rotation != "none") {
+	  rot <- do.call(rotation, c(list(load), cn$rotate))
+	  load <- if (is.list(rot)) 
+	    rot$loadings
+	  else rot
+	}
+	fit$loadings <- sortLoadings(load)
+	class(fit$loadings) <- "loadings"
+	fit$na.action <- na.act
+	if (scores != "none") {
+	  Lambda <<- fit$loadings
+	  zz <<- z
+	  switch(scores, regression = {
+	    sc <- as.matrix(zz) %*% solve(cv, Lambda)
+	    if (!is.null(Phi <- attr(Lambda, "covariance"))) 
+	      sc <- sc %*% Phi
+	  }, Bartlett = {
+	    psiinv <<- ginv(fit$psi)
+	    sc <- t(ginv(t(Lambda)%*%psiinv%*%Lambda)%*%t(Lambda)%*%psiinv%*%t(zz))
+	    ###            d <- 1/fit$uniquenesses
+	    ###            tmp <- t(Lambda * d)
+	    ###            sc <- t(solve(tmp %*% Lambda, tmp %*% t(zz)))
+	  })
+	  rownames(sc) <- rownames(z)
+	  colnames(sc) <- colnames(Lambda)
+	  if (!is.null(na.act)) 
+	    sc <- napredict(na.act, sc)
+	  fit$scores <- sc
+	}
+	if (!is.na(n.obs) && dof > 0) {
+	  fit$STATISTIC <- (n.obs - 1 - (2 * p + 5)/6 - (2 * factors)/3) * 
+	    fit$criteria["objective"]
+	  fit$PVAL <- pchisq(fit$STATISTIC, dof, lower.tail = FALSE)
+	}
+	fit$n.obs <- n.obs
+	fit$call <- cl
+	fit
 }
